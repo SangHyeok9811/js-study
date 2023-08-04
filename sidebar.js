e5bce93fd5c0ade465db91a1bd400342975e8ed3
Sanghyeok <Sanghyeok Han>
2023-08-04 18:25:25 +0900

    log in 추가

--- sidebar.js
+++ sidebar.js
@@ -3,8 +3,10 @@
   sidebar.style.position = "fixed";
   sidebar.style.height = "100vh";
   sidebar.style.width = "200px";
+  sidebar.style.marginTop = "40px";
 
   sidebar.innerHTML = /*html */ `
+    <h3 style="margin-top:0;"><a href="/">Home</a></h3>
     <ul>
       <li><a href="/todo/todo.html">To-do List</a></li>
       <li><a href="/contact/contact.html">Contacts</a></li>
