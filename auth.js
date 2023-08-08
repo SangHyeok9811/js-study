d24e9af57d9abe1016f590f5603038bc91f2b856
Sanghyeok <Sanghyeok Han>
2023-08-08 19:54:49 +0900

    08/08

--- auth.js
+++ auth.js
@@ -12,6 +12,7 @@ function getCookie(name) {
 // 인증토큰이 없으면 로그인페이지로 튕김
 (() => {
   const token = getCookie("token");
+  console.log(token);
   if (!token) {
     // 페이지를 이동시키는 window.location 객체의 속성
     window.location.href = "/login.html";
